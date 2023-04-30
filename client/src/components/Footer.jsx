d60f9b3399aa0e5f9bf4d6f915c8a2d21f246984
Shubham523 <Shubham523@github.com>
2023-04-30 14:05:39 +0530

    feat (footer) :  footer margin modify

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
     return (
-        <footer class="bg-white dark:bg-gray-900">
+        <footer class="bg-white dark:bg-gray-900 mt-10">
             <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
                 <div class="md:flex md:justify-between">
                     <div class="mb-6 md:mb-0">
